236bf2ab284ded904d6361df073b4d6c5ebbc396
Willbemax <max22042007@yandex.ru>
2024-05-20 20:50:51 +0300

    edit cors

--- middleware/cors/cors.js
+++ middleware/cors/cors.js
@@ -5,6 +5,7 @@ const ALLOWEDCORS = [
 	'https://students-projects.ru',
 	'http://localhost:3000',
 	'http://localhost:3001',
+	'https://wbm-yandex.nomoredomainswork.ru'
 ];
 
 function cors(req, res, next) {
